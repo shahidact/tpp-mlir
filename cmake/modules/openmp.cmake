04015e3efeb0504286a439a102cb676be0ba1f99
Renato Golin <rengolin@systemcall.eu>
2025-03-04 12:33:02 +0000

    [OpenMP] Rehash of #960 with GCC trick to get LLVM OMP (#1019)
    This is now more robust than my previous trick, using @dbabokin's code
    and only hacking `openmp.cmake`, not the other ones. This works on my
    machine for GCC and Clang, let's see what the CI thinks about it.
    
    Fixes #959
    
    ---------
    
    Co-authored-by: Dmitry Babokin <babokin@gmail.com>

--- cmake/modules/openmp.cmake
+++ cmake/modules/openmp.cmake
@@ -4,21 +4,40 @@ option(USE_OpenMP "Use OpenMP" ON)
 # This finds the library path from the system's clang for OpenMP
 #
 # On Fedora, it's at the same place as others, so we don't need to look elsewhere
-# On Ubuntu, it's in /usr/lib/llvm-${version}, so find_package finds GOMP for GCC instead.
-execute_process (
-    COMMAND bash -c "for lib in $(clang -lomp -### 2>&1); do echo $lib | grep -o \"\\/.*llvm.*\\w\"; done"
-    OUTPUT_VARIABLE LLVM_OMP_PATH
-)
-# Only if we found an "llvm" path that we need to add
-if (LLVM_OMP_PATH)
-  set(CMAKE_PREFIX_PATH ${LLVM_OMP_PATH})
-endif()
+# On Ubuntu, it's in /usr/lib/llvm-${version}, so GCC finds GOMP instead.
 
 if(USE_OpenMP)
-  find_package(OpenMP)
-  if(OPENMP_FOUND)
+  # Only bypass for GCC
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    # Only if we found an "llvm" path that we need to add
+    find_library(LLVM_OMP NAMES omp libomp libomp.so libomp.so.5)
+    find_library(PTHREAD NAMES pthread libpthread libpthread.a libpthread.so)
+    if (NOT LLVM_OMP OR NOT PTHREAD)
+      message(FATAL_ERROR "LLVM OpenMP required but not found")
+    endif()
+    set(OPENMP_FOUND ON)
+    set(OpenMP_C_FLAGS "-fopenmp")
+    set(OpenMP_C_LIBRARIES "${LLVM_OMP};${PTHREAD}")
+    set(OpenMP_C_LIB_NAMES "omp;pthread")
+
     message(STATUS "OpenMP found")
-  else()
-    message(FATAL_ERROR "OpenMP required but not found")
-  endif()
-endif()
+    message(STATUS "OpenMP C flags: ${OpenMP_C_FLAGS}")
+    message(STATUS "OpenMP C libs: ${OpenMP_C_LIBRARIES}")
+    message(STATUS "OpenMP C libnames: ${OpenMP_C_LIB_NAMES}")
+
+  else() # GNU / LLVM
+
+    # Clang always prefers its own OpenMP
+    find_package(OpenMP)
+    if(OPENMP_FOUND)
+      message(STATUS "OpenMP found")
+      message(STATUS "OpenMP version: ${OpenMP_C_VERSION}")
+      message(STATUS "OpenMP C flags: ${OpenMP_C_FLAGS}")
+      message(STATUS "OpenMP C libs: ${OpenMP_C_LIBRARIES}")
+      message(STATUS "OpenMP C libnames: ${OpenMP_C_LIB_NAMES}")
+    else()
+      message(FATAL_ERROR "LLVM OpenMP required but not found")
+    endif()
+
+  endif() # GNU / LLVM
+endif() # USE_OpenMP
