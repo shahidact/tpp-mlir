480883b9e4fa9b2eeb9764142658d79416b23fcb
mshahid <md.asghar.ahmad.shahid@intel.com>
2025-04-21 04:46:15 -0700

    --Remove the stricter check which was not really strict enough. --Added a TODO for better target specific pass ordering.

--- lib/TPP/PassBundles/VectorToKernel.cpp
+++ lib/TPP/PassBundles/VectorToKernel.cpp
@@ -50,10 +50,11 @@ struct VectorToKernel : public tpp::impl::VectorToKernelBase<VectorToKernel>,
 
 private:
   void constructPipeline() override {
-    // TODO: Pass ordering based on target architecture needs to be defined. For
-    // now, allow non AMX target to go through "createBF16DotProduct" pass.
-    if (!vnni::utils::hasAMX())
-      pm.addNestedPass<func::FuncOp>(createBF16DotProduct());
+    // TODO: Pass ordering based on target architecture starting from AMX ->
+    // avx512 -> avx2 to subset needs to be improved by moving out some logic of
+    // Bf16DotProduct related to iterarg creation and let hoistvectorTransfer
+    // pass address it.
+    pm.addNestedPass<func::FuncOp>(createBF16DotProduct());
     pm.addNestedPass<func::FuncOp>(createHoistVectorTransfers());
     if (vnni::utils::hasAMX())
       pm.addNestedPass<func::FuncOp>(createVectorContractToAMX());
